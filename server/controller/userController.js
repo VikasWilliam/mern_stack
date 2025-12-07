c8ce8ae60a32eede76dea1ba4b5a1e62b4b1a7db
VikasWilliam <vikasdenobili@outlook.com>
2025-12-07 13:17:57 +0530

    all operations implemented

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -11,7 +11,8 @@ export const create = async (req, res) => {
             return res.status(400).json({ errorMessage: "user already exists" })
         }
         const saveData = await newUser.save();
-        res.status(200).json(saveData)
+        // res.status(200).json(saveData)
+        res.status(200).json({ message: "User created successfully" });
 
     } catch (error) {
         res.status(500).json({ errorMessage: error.message })
